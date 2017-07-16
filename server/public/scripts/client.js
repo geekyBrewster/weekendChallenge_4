3509da3c8ae33d71aff2896d06d048227a33e595
Amanda Kirchner <geeky.brewster@gmail.com>
2017-07-16 15:29:43 -0500

    Added comments section, but comments from one image post to all images

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -5,21 +5,27 @@ var galleryApp = angular.module('galleryApp', []);
 galleryApp.controller('GalleryController', function(){
   var gallery = this;
   gallery.images = imageSource;
-  console.log(gallery.images);
+    // console.log(gallery.images);
 
   gallery.toggle = function(image) {
-    console.log("count before: ", image.count);
+    // console.log("count before: ", image.count);
     image.visibility = !image.visibility;
+
     // only count image view once when toggling from image to description and back
     if(image.visibility == true){
       image.count = image.count + 1;
     } else {
       image.count = image.count;
     }
-    console.log("count after:", image.count);
+    // console.log("count after:", image.count);
   };
 
+  gallery.addComment = function(array, comment) {
+    console.log("Comment to push: ", comment);
+    gallery.allComments = array;  // array container for all the messages appended to bottom of page
+    console.log("Current comment array:", gallery.allComments);
+    gallery.allComments.push(angular.copy(comment));
+     console.log(gallery.allComments);
+  };
 
-
-
-});
+}); //end of Gallery Controller
