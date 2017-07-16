0a33a7d058458d86560056d3ff7c5b7f9157443c
Amanda Kirchner <geeky.brewster@gmail.com>
2017-07-16 14:26:16 -0500

    Added bootstrap and some other CSS styling

---
+++ server/public/scripts/imageData.js
@@ -0,0 +1,37 @@
+var imageSource = [
+  {name: "Beer and Ultimate", source: "beerFrisbee.jpg", visibility: true, count: 0,
+  message: "Craft beer and Ultimate frisbee on a sunny day. The Twin Cities has a great beer scene, " +
+  "and there are many opportunities to play Ultimate year-round."},
+
+  {name: "Minneapolis", source: "minneapolis.jpg",  visibility: true, count: 0,
+  message: "I've had the opporunity to travel to a lot of cities in the US, " +
+  "and Minneapolis is still my favorite city."},
+
+  {name: "Lefse", source: "lefse.jpg", visibility: true, count: 0,
+  message: "There's a lot of Norwegian heritage in my family, and we would" +
+  "spend autumn weekends making lefse using sticks my grandfather carved."},
+
+  {name: "SuperCat", source: "superCat.jpg", visibility: true, count: 0,
+  message: "I like cats (and dogs), but I've had this cat for over 10 years." +
+  " And my cat likes plastic bags."},
+
+  {name: "Tokyo Tower", source: "tokyoTower.jpg", visibility: true, count: 0,
+  message: "I took a really amazing week-long trip to Japan several years ago. " +
+  "I got to see a lot of Tokyo and even took the bullet train to Kyoto."},
+
+  {name: "Stan Lee's autograph", source: "stanLee.jpg",  visibility: true, count: 0,
+  message: "For the last few years, I've volunteer at the Wizard World sci-fi conventions that " +
+  " have been through Minneapolis and Chicago. I've met several celebrities, but best of all I got to meet Stan Lee!"},
+
+  {name: "Baby blanket", source: "knitting.jpg", visibility: true, count: 0,
+  message: "I enjoy knitting for friends and family. When some close friends said " +
+  "they were expecting, I developed my first knitting pattern featuring the Hulk for them."},
+
+  {name: "Class presentation", source: "ExcelWalken.jpg", visibility: true, count: 0,
+  message: "If you asked me as a kid if I ever thought I'd be a teacher and talking in front of others, " +
+  "I definitely would have laughed and said 'No'. But now, I do enjoy it."},
+
+  {name: "Goodsell Observatory", source: "goodsellObs.jpg", visibility: true, count: 0,
+  message: "As a kid, I really loved astronomy, and growing up on a farm, I had the best views of the sky." +
+  " So I decided to study astronomy at college. And I spend many evenings in Goodsell."}
+];
